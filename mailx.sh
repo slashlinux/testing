f4591d8367e8112f77d879984da935544c6808c7
root <root@13450.k.time4vps.cloud>
2018-11-08 13:27:22 +0200

    Add all my files

---
+++ mailx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#Variables
+SFTPHOSTNAME="192.168.1.1"
+SFTPUSERNAME="user"
+SFTPPASSWORD="password"
+FOLDER="/home/$HOME/sftp"
+#SFTP CONNECTION
+output=$(/export/home/$HOME/bin/sshpass -p $SFTPPASSWORD sftp $SFTPUSERNAME@$SFTPHOSTNAME << !
+    cd $FOLDER
+    get data_output
+ exit
+!);
+list="email_address@domain.com email_address@domain.com"
+cat data_output | mailx -s "$(echo -e "Title name \nContent-Type: text/html")" -r email_address@domain.com $list
